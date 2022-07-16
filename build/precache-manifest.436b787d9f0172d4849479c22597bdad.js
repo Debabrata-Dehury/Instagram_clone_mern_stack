833f805b262b6f8bd867e9d0370bee1362873d22
Debabrata Dehury <85230620+Debabrata-Dehury@users.noreply.github.com>
2022-07-17 00:46:02 +0530

    Add files via upload

---
+++ build/precache-manifest.436b787d9f0172d4849479c22597bdad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfaf1f7ec00449eb3fe4d5e9616eb138",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b909d966f3a863dd9cbd",
+    "url": "/static/css/main.f44ac984.chunk.css"
+  },
+  {
+    "revision": "70c9184443c804ee3210",
+    "url": "/static/js/2.05df021f.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/static/js/2.05df021f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b909d966f3a863dd9cbd",
+    "url": "/static/js/main.302c9bce.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  }
+]);
---
+++ config/keys.js
@@ -0,0 +1,12 @@
+module.exports ={
+    MONGOURI:"mongodb+srv://Debabrata:eAWyj2e1o59spdn7@cluster0.5tkac.mongodb.net/?retryWrites=true&w=majority",
+    JWT_SECRET:"erechutdwsfg",
+    SENDGRID_API:"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"
+}
+
+
+// if(process.env.NODE_ENV==='production'){
+//     module.exports = require('./prod')
+// }else{
+//     module.exports = require('./dev')
+// }
